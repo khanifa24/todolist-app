0daae240e1a182768356884ebc28667deefd528e
Khanifa Muslimah Siregar <142989721+khanifa24@users.noreply.github.com>
2023-10-08 02:04:26 +0700

    push todolist app

---
+++ index.js
@@ -0,0 +1,25 @@
+//memakai dom
+// 1. pertama melakukan reference form element in js
+const formTask = document.querySelector(".form-task");
+
+//2. menambahkan event listener di element form
+formTask.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+// 3. reference html element input
+const inputForm = document.querySelector(".input-form");
+console.log(inputForm.value);
+
+// 4. reference html element ul / wrapper list
+const wrapperList = document.querySelector(".task-list-wrapper");
+
+// 5. create <li> using js
+const taskList = document.createElement("li");
+
+// 6. masukkan input value ke <li>
+taskList.innerHTML = inputForm.value;
+
+// 7. append <li> ke <ul>
+wrapperList.append(taskList);
+
+});
